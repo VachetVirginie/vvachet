7f08e1cb49cd19de51177f75d12b4ea6fb2801f7
Vivi <vachetvirginie@gmail.com>
2022-01-17 11:41:46 +0100

    Add title component

---
+++ src/components/Title.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="text-4xl text-center font-medium bold title"> {{ firstText }} <span class="text-primary">{{ secondText }}</span></div>
+</template>
+
+<script>
+export default {
+    name: "Title",
+    props: {
+        firstText:{
+            type: String,
+            required: true
+        },
+        secondText:{
+            type: String,
+            required: true
+        },
+    },
+};
+</script>
