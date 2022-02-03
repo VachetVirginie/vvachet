ab760730b717394195c5447e9bcb40dc27b1bd4b
Vivi <vachetvirginie@gmail.com>
2022-02-03 07:39:43 +0100

    wip

---
+++ src/components/SocialMediaBar.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="w-12 fixed bottom-28 left-5 hidden lg:block sm:left-8 xl:left-20 z-40">
+    <ul class="-my-4 text-center">
+      <li class="inline-block my-4">
+        <a
+          href="https://www.linkedin.com/in/vachetvirginie/"
+          target="linkedin"
+          rel="noopener noreferrer"
+          class="text-true-gray-900 transition duration-300 ease-in-out inline-block">
+          <svg
+            viewBox="0 0 24 24"
+            style="width: 48px; height: 48px;">
+            <path
+              fill="currentColor"
+              d="M20.47,2H3.53A1.45,1.45,0,0,0,2.06,3.43V20.57A1.45,1.45,0,0,0,3.53,22H20.47a1.45,1.45,0,0,0,1.47-1.43V3.43A1.45,1.45,0,0,0,20.47,2ZM8.09,18.74h-3v-9h3ZM6.59,8.48h0a1.56,1.56,0,1,1,0-3.12,1.57,1.57,0,1,1,0,3.12ZM18.91,18.74h-3V13.91c0-1.21-.43-2-1.52-2A1.65,1.65,0,0,0,12.85,13a2,2,0,0,0-.1.73v5h-3s0-8.18,0-9h3V11A3,3,0,0,1,15.46,9.5c2,0,3.45,1.29,3.45,4.06Z" />
+          </svg>
+        </a>
+      </li>
+      <li class="inline-block my-4">
+        <a
+          href="https://github.com/VachetVirginie"
+          target="github"
+          rel="noopener noreferrer"
+          class="text-true-gray-900 transition duration-300 ease-in-out inline-block">
+          <svg
+            viewBox="0 0 24 24"
+            style="width: 48px; height: 48px;"><path
+              fill="currentColor"
+              d="M12,2A10,10 0 0,0 2,12C2,16.42 4.87,20.17 8.84,21.5C9.34,21.58 9.5,21.27 9.5,21C9.5,20.77 9.5,20.14 9.5,19.31C6.73,19.91 6.14,17.97 6.14,17.97C5.68,16.81 5.03,16.5 5.03,16.5C4.12,15.88 5.1,15.9 5.1,15.9C6.1,15.97 6.63,16.93 6.63,16.93C7.5,18.45 8.97,18 9.54,17.76C9.63,17.11 9.89,16.67 10.17,16.42C7.95,16.17 5.62,15.31 5.62,11.5C5.62,10.39 6,9.5 6.65,8.79C6.55,8.54 6.2,7.5 6.75,6.15C6.75,6.15 7.59,5.88 9.5,7.17C10.29,6.95 11.15,6.84 12,6.84C12.85,6.84 13.71,6.95 14.5,7.17C16.41,5.88 17.25,6.15 17.25,6.15C17.8,7.5 17.45,8.54 17.35,8.79C18,9.5 18.38,10.39 18.38,11.5C18.38,15.32 16.04,16.16 13.81,16.41C14.17,16.72 14.5,17.33 14.5,18.26C14.5,19.6 14.5,20.68 14.5,21C14.5,21.27 14.66,21.59 15.17,21.5C19.14,20.16 22,16.42 22,12A10,10 0 0,0 12,2Z" />
+          </svg>
+        </a>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+    name: "SocialMediaBar",
+};
+</script>
