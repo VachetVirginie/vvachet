f9506f08c0d87e1a118b9344762b17821b0387d3
Vivi <vachetvirginie@gmail.com>
2022-02-12 16:51:30 +0100

    wip refacto

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -7,17 +7,21 @@
           <h1
             data-aos="fade-right"
             class="text-2xl font-bold text-primary lg:text-3xl xl:text-4xl title-min">
-            Hola <span class="inline-block pr-4 animate-wiggle">👋</span>
+            {{ datas._home.say_hello }} <span class="inline-block pr-4 animate-wiggle">👋</span>
           </h1>
           <h1
             data-aos="fade-right"
             class="text-3xl lg:text-4xl xl:text-5xl">
-            Virginie Vachet
+            {{ datas._home.name }}
           </h1>
           <p
             data-aos="fade-right"
             class="mt-5 leading-relaxed xl:w-3/4 2xl:text-xl">
-            Developpeuse web <span class="underline-effect">Front</span> et <span class="underline-effect">Back</span> end, vivant à Lyon
+            {{ datas._home.title_1 }}
+            <span class="underline-effect">{{ datas._home.title_2 }}</span>
+            {{ datas._home.title_3 }}
+            <span class="underline-effect">{{ datas._home.title_4 }}</span>
+            {{ datas._home.title_5 }}
           </p>
         </div>
       </div>
@@ -29,71 +33,64 @@
             class="w-full rounded-full"
             alt="Me"
             src="./../assets/toto.svg"
-            loading="lazy"
-            data-v-5e096964="">
+            home-skill>
         </div>
         <ul
           class="absolute top-0 left-0 w-full h-full rounded-full"
-          data-v-5e096964="">
+          home-skill>
           <li
             class="absolute flex items-center justify-center bg-white rounded-full shadow-2xl skill bg-opacity-70 dark:bg-gray-700 dark:bg-opacity-40 backdrop-filter backdrop-blur"
             title="ApiPlatform"
-            data-v-5e096964="">
+            home-skill>
             <img
               src="./../assets/technologies/webby.svg"
               alt="api platform logo"
-              loading="lazy"
-              data-v-5e096964="">
+              home-skill>
           </li>
           <li
             class="absolute flex items-center justify-center bg-white rounded-full shadow-2xl skill bg-opacity-70 dark:bg-gray-700 dark:bg-opacity-40 backdrop-filter backdrop-blur"
             title="NuxtJs"
-            data-v-5e096964="">
+            home-skill>
             <img
               src="./../assets/technologies/nuxt.svg"
               alt="nuxt logo"
-              loading="lazy"
-              data-v-5e096964="">
+              home-skill>
           </li>
           <li
             class="absolute flex items-center justify-center bg-white rounded-full shadow-2xl skill bg-opacity-70 dark:bg-gray-700 dark:bg-opacity-40 backdrop-filter backdrop-blur"
             title="Symfony"
-            data-v-5e096964="">
+            home-skill="">
             <img
               src="./../assets/technologies/symfony.svg"
               alt="symfony logo"
-              loading="lazy"
-              data-v-5e096964="">
+              home-skill>
           </li>
           <li
             class="absolute flex items-center justify-center bg-white rounded-full shadow-2xl skill bg-opacity-70 dark:bg-gray-700 dark:bg-opacity-40 backdrop-filter backdrop-blur"
             title="Vuejs"
-            data-v-5e096964="">
+            home-skill>
             <img
               src="./../assets/technologies/vue.svg"
               alt="vue logo"
-              loading="lazy"
-              data-v-5e096964="">
+              home-skill>
           </li>
           <li
             class="absolute flex items-center justify-center bg-white rounded-full shadow-2xl skill bg-opacity-70 dark:bg-gray-700 dark:bg-opacity-40 backdrop-filter backdrop-blur"
             title="Docker"
-            data-v-5e096964="">
+            home-skill>
             <img
               src="./../assets/docker.svg"
               alt="docker logo"
-              loading="lazy"
-              data-v-5e096964="">
+              home-skill>
           </li>
           <li
             class="absolute flex items-center justify-center bg-white rounded-full shadow-2xl skill bg-opacity-70 dark:bg-gray-700 dark:bg-opacity-40 backdrop-filter backdrop-blur"
             title="Tailwind"
-            data-v-5e096964="">
+            home-skill>
             <img
               src="./../assets/technologies/tailwind.svg"
               alt="tailwind logo"
-              loading="lazy"
-              data-v-5e096964="">
+              home-skill>
           </li>
         </ul>
       </div>
@@ -104,11 +101,13 @@
 <script>
 import AOS from "aos";
 import "aos/dist/aos.css";
+import text from "../text/data.json";
 export default {
     name: "Home",
     data () {
         return {
-            classes: []
+            classes: [],
+            datas: text
         };
     },
     created () {
