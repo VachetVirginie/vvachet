1db8fda397d5539026eb11470f0c5dc369cbb94a
Vivi <vachetvirginie@gmail.com>
2022-11-16 13:58:13 +0100

    add hard skills

--- src/components/Portfolio.vue
+++ src/components/Portfolio.vue
@@ -63,7 +63,8 @@
                 </p>
                 <p class="py-4 mb-4 text-base lg:text-base light-text-gray-700">
                   Mes missions principales sont: <br>
-                  - participer au développement d'interfaces tactiles développés en nuxt/vueJs/Vuetify,<br>
+                  - participer au développement d'interfaces tactiles développées en Nuxt/Vue.js 2/Vuetify,<br>
+                  - participer au développement d'un espace d'administration à destination des clients de NGTV, ainsi que de l'équipe support en Vue.js 3/PrimeVUE <br>
                   - participer au développement Api en symfony 4/5 utilisant Api platform,<br>
                   - participer à l'élaboration des tests unitaires (PHPUnit),<br>
                   - participer au suivi post mise en production.
