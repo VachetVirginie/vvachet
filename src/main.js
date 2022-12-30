bac1b59f32b092cd9fd792710c7238fd4997cf80
Vivi <vachetvirginie@gmail.com>
2022-12-30 18:40:50 +0100

    install and config

--- src/main.js
+++ src/main.js
@@ -1,9 +1,17 @@
 import { createApp } from "vue";
 import App from "./App.vue";
+import VueGtag from "vue-gtag-next";
 
 // We can import Tailwind directly here if we dont want
 // to use our own styles in /src/assets/css/app.scss
 // import 'tailwindcss/tailwind.css'
+const app = createApp(App);
+app.use(VueGtag, {
+    property: { id: "G-WF34TRY6HP" },
+    useDebugger: true,
+    isEnabled: true,
+    send_page_view: true,
+});
 
 // Import our own SCSS file which includes importing Tailwind
 import "./assets/css/app.scss";
