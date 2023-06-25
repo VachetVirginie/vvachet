44568fc1aebf8142ce18ad2fd8ba0a35469207cf
Vivi <vachetvirginie@gmail.com>
2023-06-25 12:44:57 +0200

    feat: template text in portfolio section

--- src/sections/portfolio/PortfolioSection.vue
+++ src/sections/portfolio/PortfolioSection.vue
@@ -11,10 +11,7 @@
                 data-aos="fade-right"
                 data-aos-duration="2000"
                 src="https://padelmagazine.fr/wp-content/uploads/2021/03/NGTV-Empower-Sport-Experience.jpg.webp">
-              <img
-                src="../../../public/img/backgrounds/dots.svg"
-                alt="White dots in a horizontal grid."
-                class="-mt-10 -ml-10">
+              <DotSvg class="-mt-10 -ml-10" />
             </div>
             <div
               class="w-full mt-2 ml-2 sm:w-1/2"
@@ -22,66 +19,32 @@
               data-aos-duration="2000">
               <div class="justify-between px-4 py-24 sm:flex-col">
                 <h4 class="py-4 font-medium uppercase text-primary sm:font-semibold">
-                  Ngtv Experience
+                  {{ infos._NGTV.titre }}
                 </h4> <h2 class="py-4 text-2xl font-semibold sm:text-xl lg:text-3xl light-text-gray-700 slogan">
-                  La vidéo intelligente au service du sport
+                  {{ infos._NGTV.slogan }}
                 </h2>
                 <div class="flex flex-wrap items-center justify-around p-2 mt-5 mb-5 leading-none md:p-2">
-                  <MainChips msg="JavaScript" />
-                  <MainChips msg="TypeScript" />
                   <MainChips
-                    msg="Vue.js 2 Options API" />
-                  <MainChips msg="Vue.js 3 Composition API" />
-                  <MainChips
-                    msg="Nuxt" />
-                  <MainChips
-                    msg="Vuetify" />
-                  <MainChips msg="PrimeVUE" />
-                  <MainChips
-                    msg="Symfony 4/5" />
-                  <MainChips
-                    msg="Docker" />
-                  <MainChips
-                    msg="ApiPlatform" />
-                  <MainChips
-                    msg="Mysql" />
-                  <MainChips
-                    msg="Github" />
+                    v-for="techno in infos._NGTV.technos"
+                    :key="techno"
+                    :msg="techno" />
                 </div>
                 <p class="py-4 mb-2 light-text-gray-700 text-lg">
-                  Je travaille chez Ngtv experience depuis Octobre 2019 en tant que <span class="font-bold underline-effect">developpeur web front et back </span>.
+                  {{ infos._NGTV.description_0_part_1 }}
+                  <span class="font-bold underline-effect">
+                    {{ infos._NGTV.description_0_part_2 }}
+                  </span>.
                 </p>
                 <p class="py-4 mb-2 light-text-gray-700 text-lg">
-                  NGTV est une société de services numériques. L'objectif ? Permettre aux sportifs amateurs de profiter de technologies de pointe pour améliorer leur pratique ou simplement profiter de l'expérience communautaire la plus innovante en Europe!
+                  {{ infos._NGTV.description_2 }}
                 </p>
-                <p class="py-4 mb-4 light-text-gray-700 text-lg">
-                  Mes missions principales sont: <br>
-                  - participer au développement d'interfaces tactiles développées en Nuxt/Vue.js 2/Vuetify,<br>
-                  - participer au développement d'un espace d'administration à destination des clients de NGTV, ainsi que de l'équipe support en Vue.js 3/PrimeVUE/Tailwind CSS <br>
-                  - participer au développement de sites externes de consultation de sessions en Vue.js 2/Tailwind CSS <br>
-                  - création d'un outil permettant de faire des lives sur les réseaux sociaux en Vue.js 2/OBS/OBS-websocket <br>
-                  - participer au développement Api en symfony 4/5 utilisant Api platform,<br>
-                  - participer à l'élaboration des tests unitaires (PHPUnit),<br>
-                  - participer au suivi post mise en production.
+                <p v-for="mission in infos._NGTV.missions" :key="mission" class="mb-2 text-lg light-text-gray-700">
+                  {{ mission }}
                 </p>
                 <a
                   href="https://www.ngtvexperience.com/"
                   class="flex px-10 py-3 text-xl sm:w-2/3 lg:w-1/2 text-primary">Site
-                  <svg
-                    width="24"
-                    height="24"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                    stroke-linejoin="round"
-                    stroke-linecap="round"
-                    fill="none"
-                    role="presentation"
-                    class="ml-0.5 w-6 h-6 stroke-2"><line
-                      x1="7"
-                      y1="17"
-                      x2="17"
-                      y2="7" /> <polyline points="7 7 17 7 17 17" />
-                  </svg>
+                  <siteArrow />
                 </a>
               </div>
             </div>
@@ -96,10 +59,7 @@
                 data-aos="fade-right"
                 data-aos-duration="2000"
                 src="https://pbs.twimg.com/profile_images/474222837419356161/FkijWTuB.jpeg">
-              <img
-                src="../../../public/img/backgrounds/dots.svg"
-                alt="White dots in a horizontal grid."
-                class="-mt-10 -ml-10">
+              <DotSvg class="-mt-10 -ml-10" />
             </div>
             <div
               class="w-full ml-2 sm:w-1/2"
@@ -107,52 +67,30 @@
               data-aos-duration="2000">
               <div class="justify-between px-4 py-24 sm:flex-col">
                 <h4 class="py-4 font-medium uppercase text-primary sm:font-semibold">
-                  Cegedim SRH
+                  {{ infos._Cegedim.titre }}
                 </h4> <h2 class="py-4 text-2xl font-semibold sm:text-xl lg:text-5xl light-text-gray-700 slogan">
-                  Un spécialiste des solutions et services RH
+                  {{ infos._Cegedim.slogan }}
                 </h2>
                 <div class="flex flex-wrap items-center justify-around p-2 mt-5 mb-5 leading-none md:p-4">
                   <MainChips
-                    msg="JavaScript" />
-                  <MainChips
-                    msg="TEAMSRH" />
-                  <MainChips
-                    msg="Jira" />
+                    v-for="techno in infos._Cegedim.technos"
+                    :key="techno"
+                    :msg="techno" />
                 </div>
                 <p class="py-4 mb-2 text-lg light-text-gray-700">
-                  J'ai travaillé chez Cegedim de Decembre 2018 à Octobre 2019 en tant que <span class="font-bold underline-effect">developpeur javascript</span>.
+                  {{ infos._Cegedim.description_0_part_1 }} <span class="font-bold underline-effect"> {{ infos._Cegedim.description_0_part_2 }}</span>.
                 </p>
                 <p class="py-4 mb-2 text-lg light-text-gray-700">
-                  Cegedim SRH s'appuie sur sa propre solution TEAMSRH pour offrir à ses clients des prestations d'externalisation adaptées et évolutives de leurs systèmes d'information paie et RH.
+                  {{ infos._Cegedim.description_1 }}
                 </p>
-                <p class="py-4 mb-2 text-lg light-text-gray-700">
-                  Mes missions principales étaient: <br>
-                  - Participer à l’élaboration de l’analyse fonctionnelle et technique chez le client, <br>
-                  - Valider une solution technique à partir de l’analyse fonctionnelle,<br>
-                  - Rédiger le cahier des charges, <br>
-                  - Assurer le paramétrage (ou le développement spécifique si nécessaire), <br>
-                  - Réaliser les tests unitaires, <br>
-                  - Participer à l’intégration de la solution (scenarii de tests…), <br>
-                  - Présenter un point d’avancement (demandes d’arbitrage, difficultés rencontrées, etc) lors des revues avec le Chef de Projet. <br>
+                <p v-for="mission in infos._Cegedim.missions" :key="mission" class="mb-2 text-lg light-text-gray-700">
+                  {{ mission }}
                 </p>
                 <a
                   href="https://www.cegedim-srh.com/Pages/Accueil.aspx"
                   class="flex px-10 py-3 text-xl sm:w-2/3 lg:w-1/2 text-primary">Site
-                  <svg
-                    width="24"
-                    height="24"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                    stroke-linejoin="round"
-                    stroke-linecap="round"
-                    fill="none"
-                    role="presentation"
-                    class="ml-0.5 w-6 h-6 stroke-2"><line
-                      x1="7"
-                      y1="17"
-                      x2="17"
-                      y2="7" /> <polyline points="7 7 17 7 17 17" />
-                  </svg></a>
+                  <siteArrow />
+                </a>
               </div>
             </div>
           </div>
@@ -166,10 +104,7 @@
                 data-aos="fade-right"
                 data-aos-duration="2000"
                 src="https://cdn.1min30.com/wp-content/uploads/2017/07/Orange-logo-scaled.jpg">
-              <img
-                src="../../../public/img/backgrounds/dots.svg"
-                alt="dots in a horizontal grid."
-                class="-mt-10 -ml-10">
+              <DotSvg class="-mt-10 -ml-10" />
             </div>
             <div
               class="w-full ml-2 sm:w-1/2"
@@ -177,47 +112,27 @@
               data-aos-duration="2000">
               <div class="justify-between px-4 py-24 sm:flex-col">
                 <h4 class="py-4 font-medium uppercase text-primary sm:font-semibold">
-                  Orange
+                  {{ infos._Orange.titre }}
                 </h4>
                 <h2 class="py-4 text-2xl font-semibold sm:text-xl lg:text-5xl light-text-gray-700 slogan">
-                  La vie change avec Orange
+                  {{ infos._Orange.slogan }}
                 </h2>
                 <div class="flex flex-wrap items-center justify-around p-2 mt-5 mb-5 leading-none md:p-4">
                   <MainChips
-                    msg="Php" />
-                  <MainChips
-                    msg="Jquery" />
-                  <MainChips
-                    msg="Zend 1" />
-                  <MainChips
-                    msg="Tortoise SVN" />
+                    v-for="techno in infos._Orange.technos"
+                    :key="techno"
+                    :msg="techno" />
                 </div>
                 <p class="py-4 mb-2 text-lg light-text-gray-700">
-                  J'ai travaillé chez Orange de Decembre 2017 à Decembre 2018 en tant que <span class="font-bold underline-effect">alternante en backend</span> au sein de la DSI.
+                  {{ infos._Orange.description_0_part_1 }} <span class="font-bold underline-effect"> {{ infos._Orange.description_0_part_2 }}</span>  {{ infos._Orange.description_0_part_3 }}
                 </p>
-                <p class="py-4 mb-2 text-lg light-text-gray-700">
-                  Mes missions principales étaient: <br>
-                  – Comprendre les besoins métiers et les reformuler par des solutions techniques, <br>
-                  – Concevoir et développer les fonctionnalités.
+                <p v-for="mission in infos._Cegedim.missions" :key="mission" class="mb-2 text-lg light-text-gray-700">
+                  {{ mission }}
                 </p>
                 <a
                   href="https://www.orange.com/fr"
                   class="flex px-10 py-3 text-xl sm:w-2/3 lg:w-1/2 text-primary">Site
-                  <svg
-                    width="24"
-                    height="24"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                    stroke-linejoin="round"
-                    stroke-linecap="round"
-                    fill="none"
-                    role="presentation"
-                    class="ml-0.5 w-6 h-6 stroke-2"><line
-                      x1="7"
-                      y1="17"
-                      x2="17"
-                      y2="7" /> <polyline points="7 7 17 7 17 17" />
-                  </svg>
+                  <siteArrow />
                 </a>
               </div>
             </div>
@@ -230,17 +145,27 @@
 
 <script>
 import MainChips from "../../components/commons/MainChips.vue";
+import DotSvg from "./assets/dotsSvg.vue";
+import siteArrow from "./assets/siteArrow.vue";
+import infos from "./data.json";
 
 export default {
     name: "Portfolio",
     components: {
         MainChips,
+        DotSvg,
+        siteArrow
     },
     props: {
         msg:{
             type: String,
             required: true
         }
+    },
+    data() {
+        return {
+            infos: infos
+        };
     }
 };
 </script>
