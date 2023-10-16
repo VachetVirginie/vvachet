c482cc03b620f23e066526bf8e3f27d5ec741670
Vivi <vachetvirginie@gmail.com>
2023-10-16 17:06:48 +0200

    remove console.log

--- src/sections/portfolio/assets/dotsSvg.vue
+++ src/sections/portfolio/assets/dotsSvg.vue
@@ -334,7 +334,6 @@ export default {
             const colorMap = {
                 secondary: "fill-secondary",
             };
-            console.log(this.color);
             return colorMap[this.color] || colorMap.secondary;
         }
     }
